0f471282fb305e475e58755183a073b8c9c5be57
Niko <niko.k.mobile@gmail.com>
2025-05-08 09:45:36 +0200

    Ajout de PWA

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {fileURLToPath, URL} from 'node:url'
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        VitePWA({registerType: "autoUpdate"})
+    ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
