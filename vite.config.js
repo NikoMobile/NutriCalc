2fcde0ab8b1fa6277cc3c61382575419d54c420e
Niko <niko.k.mobile@gmail.com>
2025-05-09 07:40:28 +0200

    Modification du PWA : ajout des icons et des couleurs

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,28 @@ import svgLoader from "vite-svg-loader";
 export default defineConfig({
     plugins: [
         vue(),
-        VitePWA({registerType: "autoUpdate"}),
+        VitePWA({
+            registerType: "autoUpdate",
+            includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+            manifest: {
+                name: 'NutriCalc',
+                short_name: 'NutriCalc',
+                description: 'Petite calculette pour nutritionnistes',
+                theme_color: '#699f8e',
+                icons: [
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+
+                    }]
+            }
+        }),
         svgLoader()
     ],
     resolve: {
